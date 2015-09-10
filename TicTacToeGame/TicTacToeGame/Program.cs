2aefb4ebfa8d838807eb70a13e91b7a299b7493d
Sai <andol.sairam@gmail.com>
2015-09-10 23:45:00 +0100

    Initialize TicTacToeGame solution containing two projects, a console app and a test project

---
+++ TicTacToeGame/TicTacToeGame/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TicTacToeGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Tic Tac Toe";
+            Console.WriteLine("Welcome to the Tic Tac Toe Game!");
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("Press enter to start a new game:");
+            Console.ReadKey(false);
+
+        }
+    }
+}
